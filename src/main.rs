dab51917023beda3ad8fff53f5664f62284de5f7
Angus Mason <angus.mason@me.com>
2024-03-14 17:29:18 +1100

    Working yew kinda

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,16 @@ use yew::prelude::*;
 #[function_component(App)]
 fn app() -> Html {
     html! {
-        <h1>{ "Hello World" }</h1>
+        <>
+            <h1>{ "RustConf Explorer" }</h1>
+            <div>
+                <h3>{"Videos to watch"}</h3>
+                <p>{ "John Doe: Building and breaking things" }</p>
+                <p>{ "Jane Smith: The development process" }</p>
+                <p>{ "Matt Miller: The Web 7.0" }</p>
+                <p>{ "Tom Jerry: Mouseless development" }</p>
+            </div>
+        </>
     }
 }
 
