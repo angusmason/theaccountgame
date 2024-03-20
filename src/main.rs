7d3bf364c4cf4d0bd0c808b0e45cee45f404670f
Matthew Li <matthew.li10@education.nsw.gov.au>
2024-03-20 18:47:56 +1100

    Refactor password input styling

--- src/main.rs
+++ src/main.rs
@@ -39,21 +39,31 @@ fn App() -> Html {
         <main class="grid place-content-center h-full grid-cols-3">
             <div></div>
             <div class="flex flex-col divide-y">
-                <div class="flex flex-col gap-4 p-4 relative z-0">
+                <div class="flex flex-col gap-4 p-4">
                     <h1 class="text-2xl font-semibold">
                         {"Create a password."}
                     </h1>
-                    <input
-                        type="password"
-                        id="floating_password"
-                        class="block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-transparent rounded-lg border-1 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-                        placeholder=" "
-                        {oninput}
-                    />
-                    <label
-                        for="floating_password"
-                        class="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white dark:bg-gray-900 px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1"
-                    >{"Password"}</label>
+                    <div class="relative">
+                        <input
+                            type="password"
+                            id="password"
+                            placeholder=" "
+                            class="block px-2.5 pb-2.5 pt-4 w-full text-sm rounded-lg border
+                                border-gray-300 focus:ring-0 focus:border-blue-600 peer"
+                            {oninput}
+                        />
+                        <label
+                            for="password"
+                            class="absolute text-gray-400 duration-300 top-1 scale-75 left-2
+                                -translate-y-4
+                                bg-white px-2 peer-focus:text-blue-600
+                                origin-left peer-placeholder-shown:scale-100
+                                peer-focus:top-1 peer-placeholder-shown:top-1/2 peer-focus:scale-75
+                                peer-focus:-translate-y-4 peer-placeholder-shown:-translate-y-3"
+                        >
+                            {"Password"}
+                        </label>
+                    </div>
                 </div>
                 <div class="flex flex-col gap-4 p-4">
                     <p class="text-1xl text-red-500 bg-red-200 rounded-xl p-4">
