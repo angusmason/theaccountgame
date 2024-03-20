1a32999f177a33182143de548d19cec1cefa51b5
Angus Mason <angus.mason@me.com>
2024-03-20 18:13:59 +1100

    Looking nice

--- src/main.rs
+++ src/main.rs
@@ -56,7 +56,7 @@ fn App() -> Html {
                         class="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white dark:bg-gray-900 px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1"
                     >{"Password"}</label>
                 </div>
-                <div>
+                <div class="flex flex-col gap-4 p-4">
                     <p class="text-1xl text-red-500 bg-red-200 rounded-xl p-4">
                         {
                             // Map the wrong message to a HTML element
