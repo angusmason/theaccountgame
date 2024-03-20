f2fcf174d66db4a980d9fa626e71e0e31d76ee5b
Matthew Li <matthew.li10@education.nsw.gov.au>
2024-03-20 15:54:22 +1100

    Update .gitignore and Cargo.toml, delete unused files

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,25 @@
 #![warn(clippy::pedantic, clippy::nursery)]
 
-mod conditions;
+use web_sys::HtmlInputElement;
+use yew::{classes, function_component, html, use_state, Html, InputEvent, TargetCast};
 
-use conditions::conditions;
-use rpassword::prompt_password;
-use yew::prelude::*;
-
-#[function_component(App)]
-fn app() -> Html {
+#[function_component]
+fn App() -> Html {
+    let input = use_state(String::new);
+    let oninput = {
+        let input = input.clone();
+        move |event: InputEvent| {
+            input.set(event.target_dyn_into::<HtmlInputElement>().unwrap().value());
+        }
+    };
     html! {
-        <>
-            <h1>{ "RustConf Explorer" }</h1>
-            <div>
-                <h3>{"Videos to watch"}</h3>
-                <p>{ "John Doe: Building and breaking things" }</p>
-                <p>{ "Jane Smith: The development process" }</p>
-                <p>{ "Matt Miller: The Web 7.0" }</p>
-                <p>{ "Tom Jerry: Mouseless development" }</p>
-            </div>
-        </>
+        <div class={classes!("")}>
+            {"Please choose a password."}
+            <input type="password" {oninput}/>
+        </div>
     }
 }
 
 fn main() {
     yew::Renderer::<App>::new().render();
-
-    let conditions = conditions();
-    let password = 'outer: loop {
-        let password = prompt_password("\nChoose a password: ")
-            .unwrap()
-            .trim()
-            .to_string();
-        for (condition, message) in &conditions {
-            if !condition(&password) {
-                println!("{message}");
-                continue 'outer;
-            }
-        }
-        println!("Password meets requirements.");
-        break password;
-    };
-
-    loop {
-        let password_confirmation = prompt_password("\nConfirm your password: ")
-            .unwrap()
-            .trim()
-            .to_string();
-        if password_confirmation == password {
-            println!("\nPassword matches.");
-            break;
-        }
-        println!("Password does not match.");
-    }
 }
