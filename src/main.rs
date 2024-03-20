996e569ea24f18dbb01ae4791af1cd12381c07d7
Angus Mason <angus.mason@me.com>
2024-03-21 09:28:18 +1100

    Make gap smaller

--- src/main.rs
+++ src/main.rs
@@ -39,7 +39,7 @@ fn App() -> Html {
         <main class="grid place-content-center h-full grid-cols-3">
             <div></div>
             <div class="flex flex-col">
-                <div class="flex flex-col gap-4 p-4">
+                <div class="flex flex-col gap-4 px-4 pb-4">
                     <h1 class="text-2xl font-semibold">
                         {"Create a password."}
                     </h1>
@@ -64,7 +64,7 @@ fn App() -> Html {
                         </label>
                     </div>
                 </div>
-                <div class="flex flex-col p-4">
+                <div class="flex flex-col px-4 pt-4">
                     {
                         // Map the wrong message to a HTML element
                         // If it was Some, it will map to a paragraph with the message
