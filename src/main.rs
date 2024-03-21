e5f0e0918f1764c836e3933154a56a9a74164c3e
Matthew Li <matthew.li10@education.nsw.gov.au>
2024-03-21 19:15:27 +1100

    Refactor main layout styling and fix message mapping logic

--- src/main.rs
+++ src/main.rs
@@ -36,8 +36,8 @@ fn App() -> Html {
 
     // Return some HTML
     html! {
-        <main class="flex md:justify-center h-full">
-            <div class="flex flex-col items-center justify-center h-full w-full md:w-1/3 px-4">
+        <main class="flex justify-center h-full">
+            <div class="flex flex-col items-center justify-center h-full max-w-4xl w-full px-4">
                 <div class="flex flex-col gap-4 relative w-full">
                     <h1 class="text-2xl font-semibold">
                         {"Create a password."}
