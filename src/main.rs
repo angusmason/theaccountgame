1445db484915fc219d9ac8bdc1bf86f5a4e640fe
Matthew Li <matthew.li10@education.nsw.gov.au>
2024-03-22 13:01:37 +1100

    Fix wrong message mapping logic in main.rs

--- src/main.rs
+++ src/main.rs
@@ -151,7 +151,7 @@ fn App() -> Html {
                         <button
                             disabled={confirm != password}
                             class="disabled:opacity-20 border-gray-700 border p-2 rounded-xl
-                                bg-w hover:bg-gray-200 transition"
+                                hover:bg-gray-200 transition"
                         >
                             {"Submit"}
                         </button>
