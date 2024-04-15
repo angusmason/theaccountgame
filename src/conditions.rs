c19e3d8ce2978944e865d9afb99745f7c3515d71
Matthew Li <matthew.li10@education.nsw.gov.au>
2024-04-15 14:28:19 +1000

    Refactor CSS classes in main.rs for improved readability and consistency

--- src/conditions.rs
+++ src/conditions.rs
@@ -1,7 +1,10 @@
+use std::iter::successors;
+
 use chrono::Local;
 use rand::thread_rng;
 use rand::{prelude::SliceRandom, Rng};
-use yew::{html, Html};
+use yew::virtual_dom::VNode;
+use yew::{classes, html, Html};
 
 pub type Condition = (Box<dyn Fn(&String, &String) -> bool>, Html);
 enum Colour {
@@ -15,10 +18,9 @@ pub fn conditions() -> Vec<Condition> {
     let numbers: Vec<_> = include_str!("numbers").trim().split('\n').collect();
     let vec = vec![
         (
-            Box::new(
-                |_username: &String, password: &String|
-                    !password.to_lowercase().contains("bean")
-            ) as Box<dyn Fn(&String, &String) -> bool>,
+            Box::new(|_username: &String, password: &String| {
+                !password.to_lowercase().contains("bean")
+            }) as Box<dyn Fn(&String, &String) -> bool>,
             "Password may not contain the phrase 'bean'.".into(),
         ),
         {
@@ -96,7 +98,7 @@ pub fn conditions() -> Vec<Condition> {
                     <div class="flex flex-col gap-4">
                         <p>{"Password must contain the answer to this Wordle."}</p>
                         <div class="flex">
-                            <div class="text-white bg-slate-600 p-2">
+                            <div class="p-2 text-white bg-slate-600">
                                 {
                                     words.iter().map(|word| {
                                         html! {
@@ -105,24 +107,21 @@ pub fn conditions() -> Vec<Condition> {
                                                     match colour {
                                                         Colour::Grey => html! {
                                                             <div
-                                                                class="bg-gray-500 w-8 grid
-                                                                    place-content-center"
+                                                                class="grid w-8 bg-gray-500 // place-content-center"
                                                             >
                                                                 {character.to_string().to_uppercase()}
                                                             </div>
                                                         },
                                                         Colour::Yellow => html! {
                                                             <div
-                                                                class="bg-yellow-500 w-8 grid
-                                                                    place-content-center"
+                                                                class="grid w-8 bg-yellow-500 // place-content-center"
                                                             >
                                                                 {character.to_string().to_uppercase()}
                                                             </div>
                                                         },
                                                         Colour::Green => html! {
                                                             <div
-                                                                class="bg-green-500 w-8 grid
-                                                                    place-content-center"
+                                                                class="grid w-8 bg-green-500 // place-content-center"
                                                             >
                                                                 {character.to_string().to_uppercase()}
                                                             </div>
@@ -182,7 +181,7 @@ pub fn conditions() -> Vec<Condition> {
                     <div class="flex flex-col gap-4">
                         <p>{"Password must contain the 24-bit hexadecimal colour of this box."}</p>
                         <div
-                            class="w-32 h-32 border-slate-600 border-8"
+                            class="w-32 h-32 border-8 border-slate-600"
                             style={format!("background-color: #{hex}")}
                         />
                     </div>
@@ -244,7 +243,157 @@ pub fn conditions() -> Vec<Condition> {
         (
             Box::new(|_username, password| ('\u{1F3FB}'..='\u{1F3FF}').all(|char| password.contains(char))),
             "Password must be ethnically diverse. 👍".into()
-        )
+        ),
+        {
+            const MAZE_SIZE: u32 = 20;
+            const BORDER_WIDTH: u32 = 1;
+            type Cell = (Position, bool);
+            type Position = (u32, u32);
+            fn neighbours((x, y): Position) -> Vec<Position> {
+                vec![
+                    (x, y.wrapping_sub(1)),
+                    (x.wrapping_add(1), y),
+                    (x, y.wrapping_add(1)),
+                    (x.wrapping_sub(1), y),
+                ]
+            }
+            #[repr(u8)]
+            enum Direction {
+                Up = b'R',
+                Right = b'D',
+                Down = b'L',
+                Left = b'U',
+            }
+
+            let mut maze: Vec<_> = (0..MAZE_SIZE)
+                .flat_map(|y| (0..MAZE_SIZE).map(move |x| ((x, y), false) as Cell))
+                .collect();
+            let mut stack = Vec::new();
+            let mut paths = Vec::new();
+            let cell = (0, 0);
+            let (_, visited) = maze
+                .iter_mut()
+                .find(|(position, _)| *position == cell)
+                .unwrap();
+            *visited = true;
+            stack.push(cell);
+            while let Some(cell) = stack.pop() {
+                let neighbours: Vec<_> = neighbours(cell)
+                    .into_iter()
+                    .filter_map(|neighbour| {
+                        maze.iter().find_map(|(position, visited)| {
+                            (*position == neighbour && !visited).then_some(*position)
+                        })
+                    })
+                    .collect();
+                if let Some(neighbour) = neighbours.choose(&mut thread_rng()) {
+                    stack.push(cell);
+                    paths.push((cell, *neighbour));
+                    let (_, visited) = maze
+                        .iter_mut()
+                        .find(|(position, _)| *position == *neighbour)
+                        .unwrap();
+                    *visited = true;
+                    stack.push(*neighbour);
+                }
+            }
+            let (solution, start, goal) = {
+                let maze: Vec<_> = maze.iter().map(|(position, _)| position).copied().collect();
+                let cell = *maze.choose(&mut thread_rng()).unwrap();
+                let goal = *maze.choose(&mut thread_rng()).unwrap();
+                let mut queue = vec![cell];
+                let mut explored = vec![cell];
+                let mut links = Vec::new();
+                while let Some(cell) = queue.pop() {
+                    if cell == goal {
+                        break;
+                    }
+                    for neighbour in neighbours(cell) {
+                        if maze.contains(&neighbour)
+                            && !explored.contains(&neighbour)
+                            && paths.iter().any(|(a, b)| {
+                                *a == cell && *b == neighbour || *a == neighbour && *b == cell
+                            })
+                        {
+                            queue.push(neighbour);
+                            links.push((neighbour, cell));
+                            explored.push(neighbour);
+                        }
+                    }
+                }
+                let path: Vec<_> = successors(Some(goal), move |&cell| {
+                    links
+                        .iter()
+                        .find_map(|(to, from)| (*to == cell).then_some(*from))
+                })
+                .collect();
+                let path: Vec<_> = path.iter().copied().rev().collect();
+                (path, cell, goal)
+            };
+            let solution: String = solution
+                .windows(2)
+                .map(|window| {
+                    let [from, to] = window else { unreachable!() };
+                    (if from.0 < to.0 {
+                        Direction::Right
+                    } else if from.0 > to.0 {
+                        Direction::Left
+                    } else if from.1 < to.1 {
+                        Direction::Down
+                    } else {
+                        Direction::Up
+                    }) as u8 as char
+                })
+                .collect();
+            let maze = (0..MAZE_SIZE)
+                .flat_map(|y| {
+                    let paths = paths.clone();
+                    (0..MAZE_SIZE).map(move |x| {
+                        let neighbours: Vec<_> = neighbours((x, y))
+                            .iter()
+                            .map(|neighbour| {
+                                format!(
+                                    "{}px",
+                                    if paths.iter().any(|(from, to)| {
+                                        (*from == (x, y) && *to == *neighbour)
+                                            || (*from == *neighbour && *to == (x, y))
+                                    }) {
+                                        0
+                                    } else {
+                                        BORDER_WIDTH
+                                    }
+                                )
+                            })
+                            .collect();
+                        let borders = neighbours.join(" ");
+                        html! {
+                            <div
+                                style={format!("border-width: {borders}")}
+                                class={classes!(
+                                    "border-white", "size-full",
+                                    ((x, y) == start).then_some("bg-green-500"),
+                                    ((x, y) == goal).then_some("bg-red-500"),
+                                )}
+                            />
+                        }
+                    })
+                })
+                .collect::<VNode>();
+            (
+                Box::new(move |_username, password| password.contains(&solution)),
+                html! {
+                    <div>
+                        <p>{"Password must contain the optimal solution to this maze, from green to red."}</p>
+                        <p>{"R is up, D is right, L is down, U is left."}</p>
+                        <div class="p-4 aspect-square bg-slate-600">
+                            <div class="grid grid-cols-[repeat(20,minmax(0,1fr))] size-full">
+                                {maze}
+                            </div>
+                        </div>
+                    </div>
+                },
+            )
+        },
     ];
     vec
 }
